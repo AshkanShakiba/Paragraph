7eab2237501515317f764c1af96a96750abce313
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2022-09-07 22:18:04 +0430

    set views permissions

--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,16 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
